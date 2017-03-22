e7dea160c83c069935ba80312ad7d22d5fee9540
osboxes.org <osboxes.henrygarcia0309@gmail.com>
2017-03-21 21:09:57 -0500

    car fault method private

--- car.rb
+++ car.rb
@@ -1,30 +1,47 @@
-system("clear")
-
 class Car
-  attr_accessor :speed, :acceleration, :braking
+  attr_accessor :acceleration, :braking, :velocity
 
-  def initialize
-    @velocity = 0
+  def initialize(velocity = 0)
+    @velocity = velocity
+    @acceleration = 0
   end
 
-  def velocity
-    @velocity
+  def accelerate(acceleration=nil)
+    if  acceleration.nil?
+        @velocity = @velocity + 1
+    else
+       @acceleration = acceleration
+       @velocity = @velocity + @acceleration
+    end
   end
 
-  def accelerate (*acceleration)
-    acceleration.length == 0 ?  @velocity = @velocity + 1 : @velocity = @velocity + acceleration.first
-  end
+  def brake(braking=nil)
+    if braking.nil?
+       @velocity =   @velocity  - 1
+    else
+      @braking = braking
+      @velocity = @velocity - @braking
+    end
+ end
 
-  def brake
-    @velocity =   @velocity  - 1
-  end
+    def velocity
+      show_velocity
+    end
 
-  def brake braking
-     @velocity = @velocity - braking
-  end
+    protected
+      def show_velocity
+        @velocity
+      end
 end
 
 car = Car.new
-car.velocity(3)
-car.accelerate
-puts car.inspect
+p car.velocity = 10
+p car.accelerate(66)
+p car.velocity
+#p car.accelerate
+#p car.accelerate
+#p car.velocity
+#p car.accelerate
+#p car.brake
+p car.brake(12)
+p car.velocity
